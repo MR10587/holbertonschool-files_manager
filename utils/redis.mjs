e97ef452efca6edf2eceb3f12d3d8ccd310ad29f
MR10587 <ramalmirzezade20@gmail.com>
2026-07-09 09:51:04 +0000

    .

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,10 +1,10 @@
-import redis from "redis";
+import redis from 'redis';
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
 
-    this.client.on("error", (err) => {
+    this.client.on('error', (err) => {
       console.error(err);
     });
   }
@@ -16,8 +16,10 @@ class RedisClient {
   async get(key) {
     return new Promise((resolve, reject) => {
       this.client.get(key, (err, value) => {
-        if (err) return reject(err);
-        resolve(value);
+        if (err) {
+          return reject(err);
+        }
+        return resolve(value);
       });
     });
   }
@@ -25,8 +27,10 @@ class RedisClient {
   async set(key, value, duration) {
     return new Promise((resolve, reject) => {
       this.client.setex(key, duration, value, (err, reply) => {
-        if (err) return reject(err);
-        resolve(reply);
+        if (err) {
+          return reject(err);
+        }
+        return resolve(reply);
       });
     });
   }
@@ -34,8 +38,10 @@ class RedisClient {
   async del(key) {
     return new Promise((resolve, reject) => {
       this.client.del(key, (err, reply) => {
-        if (err) return reject(err);
-        resolve(reply);
+        if (err) {
+          return reject(err);
+        }
+        return resolve(reply);
       });
     });
   }
