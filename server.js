79e495dceb369e5543eb1188b53a9e94007daf04
MR10587 <ramalmirzezade20@gmail.com>
2026-07-10 22:11:39 +0400

    .

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-import express from 'express';
-import router from './routes/index';
+import express from "express";
+import router from "./routes/index";
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use('/', router);
+app.use(express.json);
+
+app.use("/", router);
 
 app.listen(port);
