009ce20825e280b2b177b2d810032a619bf77266
MR10587 <ramalmirzezade20@gmail.com>
2026-07-10 22:06:16 +0400

    .

---
+++ controllers/UsersController.js
@@ -0,0 +1,35 @@
+import dbClient from "../utils/db";
+import crypto from "crypto";
+
+export const postNew = async function (req, res) {
+  const usersCollection = dbClient.db.collection("users");
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: "Missing email" });
+  }
+  if (!password) {
+    return res.status(400).json({ error: "Missing password" });
+  }
+
+  const user = await usersCollection.findOne({ email });
+
+  if (user) {
+    return res.status(400).json({ error: "Already exist" });
+  }
+
+  const hashedPassword = crypto
+    .createHash("sha1")
+    .update(password)
+    .digest("hex");
+
+  const newUser = await usersCollection.insertOne({
+    email,
+    password: hashedPassword,
+  });
+
+  return res.status(201).json({
+    id: newUser.insertedId.toString(),
+    email,
+  });
+};
