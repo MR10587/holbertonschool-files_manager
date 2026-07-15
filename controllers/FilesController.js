e788c76a6388b2f33c241fb501af98c4dd4190e3
MR10587 <ramalmirzezade20@gmail.com>
2026-07-15 12:09:20 +0400

    .

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -123,23 +123,26 @@ export const getShow = async (req, res) => {
     });
   }
 
-  const file = await filesCollection.findOne({ id });
+  const file = await filesCollection.findOne({
+    _id: new ObjectId(id),
+    userId: user,
+  });
 
   if (!file) {
     return res.status(404).json({
-      error: 'Not found'
+      error: "Not found",
     });
   }
 
-  return res.status(200).json(file)
+  return res.status(200).json(file);
 };
 
-export const getIndex = async(req, res) => {
+export const getIndex = async (req, res) => {
   const token = req.header("X-Token");
   const user = await redisClient.get(`auth_${token}`);
   const filesCollection = dbClient.db.collection("files");
   let parentId = req.query.parentId;
-  const pages = req.query.pages;
+  const page = Number(req.query.page || 0);
 
   if (!user) {
     return res.status(401).json({
@@ -149,7 +152,18 @@ export const getIndex = async(req, res) => {
 
   if (!parentId) {
     parentId = 0;
+  } else {
+    parentId = new ObjectId(parentId);
   }
 
-  return await filesCollection.findOne({ parentId }) || [];
-}
+  const files = await filesCollection
+    .find({
+      userId: user,
+      parentId,
+    })
+    .skip(page * 20)
+    .limit(20)
+    .toArray();
+
+  return res.status(200).json(files);
+};
